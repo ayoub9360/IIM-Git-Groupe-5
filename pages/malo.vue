5baa0403a15ce48f135c55f24d543030612dad66
Braunschweig Malo <malo.braunschweig@live.fr>
2021-09-21 16:22:55 +0200

    Create malo page

---
+++ pages/malo.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="malo">
+    <h1>Page de Malo</h1>
+  </div>
+</template>
+<script>
+    export default{}
+</script>
+<style>
+  body {
+    margin: 0;
+    font-family: Arial;
+  }
+</style>
