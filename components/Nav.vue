67d63ca7971dcc7fb1ccb9bb18ba477f984788f4
Ayoub El Guendouz <ayoub.el_guendouz@edu.devinci.fr>
2021-09-21 17:55:27 +0200

    feat(Page): :rocket: Add Ayoub Page (#6)
    * feat(Ayoub Page): :rocket: Add Ayoub Page
    
    * fix(Page): :bug: Fix link

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="nav">
-    <nuxt-link to="Kevin">Kevin</nuxt-link>
-    <nuxt-link to="malo">malo</nuxt-link>
-    <nuxt-link to="tom">tom</nuxt-link>
+    <nuxt-link to="/" class="text-green-500 hover:underline">Accueil</nuxt-link>
+    <nuxt-link to="Kevin" class="text-green-500 hover:underline">Kevin</nuxt-link>
+    <nuxt-link to="malo" class="text-green-500 hover:underline">Malo</nuxt-link>
+    <nuxt-link to="tom" class="text-green-500 hover:underline">Tom</nuxt-link>
+    <nuxt-link to="mathilde" class="text-green-500 hover:underline">Mathilde</nuxt-link>
+    <nuxt-link to="ayoub" class="text-green-500 hover:underline">Ayoub</nuxt-link>
   </div>
 </template>
 
@@ -12,7 +15,7 @@
     justify-content: space-around;
     align-items: center;
     min-height: 70px;
-    background: rgb(216, 216, 216);
+    background: rgb(207, 191, 193);
   }
 
   a {
