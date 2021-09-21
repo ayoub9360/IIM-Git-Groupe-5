dd52f8f946c1edfc43f4ed507bbd9a44fcbda8a4
TomymyFr <tomybrz@hotmail.fr>
2021-09-21 23:51:47 +0200

    malo : fix

--- pages/malo.vue
+++ pages/malo.vue
@@ -1,9 +1,9 @@
 <template>
-  <div id="malo" class=" flex justify-center bg-gradient-to-r from-yellow-300 to-indigo-400 p-2 sm:items-center sm:pt-0"
+  <div id="malo" class="flex justify-center bg-gradient-to-r from-yellow-300 to-indigo-400 p-2 sm:items-center sm:pt-0"
     style="height: calc(100vh - 70px)">
     <div class="mx-auto sm:px-6 lg:px-8 container">
       <div class="mt-8 bg-white overflow-hidden shadow sm:rounded-lg p-6">
-        <a class="float-right p-2 bg-green-500 text-white rounded-lg shadow-md" href="https://bit.ly/3AAseaI" target="_blank" rel="noreferrer noopener">Vidéo de présentation</a>
+        <a class="float-right p-2 bg-yellow-600 text-white rounded-lg shadow-md" href="https://bit.ly/3AAseaI" target="_blank" rel="noreferrer noopener">September 🎶</a>
         <h1 class="text-2xl leading-7 font-semibold">Page de Malo</h1>
       </div>
     </div>
