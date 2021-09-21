bec1e9cec2e1e2c1fb03150e4ab10d77d6e7ed13
Ayoub El Guendouz <ayoub.el_guendouz@edu.devinci.fr>
2021-09-21 17:43:54 +0200

    feat(Ayoub Page): :rocket: Add Ayoub Page

---
+++ pages/ayoub.vue
@@ -0,0 +1,59 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css" rel="stylesheet">
+    <div style="width: 100%" class='container'>
+      <div class="mt-8 bg-white overflow-hidden shadow sm:rounded-lg p-6">
+        <h2 class="text-2xl leading-7 font-semibold">
+          Hi, I'm El Guendouz Ayoub 👋
+        </h2>
+        <p class="mt-3 text-gray-600">
+          I am a third year student in web development !
+        </p>
+        <p class="mt-4 pt-4 text-gray-800 border-t border-dashed">
+          <ul>
+            <li>🔭   I’m currently working at <a href="www.spendesk.com"
+                class="text-purple-500 hover:underline">Spendesk</a></li>
+            <li>👨‍💻   All of my projects are available in my <a href="www.elguendouz-ayoub.com"
+                class="text-green-500 hover:underline">Website</a></li>
+            <li>📫   How to reach me: ayoub.elguendouz[@]gmail .com</li>
+            <li>📄   <a href="https://elguendouz-ayoub.com/assets/El_Guendouz_Ayoub_CV.pdf">Know about my experiences
+                with my resume</a></li>
+          </ul>
+        </p>
+        <p class="mt-4 pt-4 text-gray-800 border-t border-dashed">
+          <h2 class="pb-4 text-2xl leading-7 font-semibold">
+            My last project
+          </h2>
+          <ul>
+            <li><a href="https://github.com/ayoub9360/responsive-bot-check" class="text-green-500 hover:underline"> -
+                Responsive Check Bot</a></li>
+            <li><a href="https://yibzee.com/" class="text-green-500 hover:underline"> - Yibzee</a></li>
+            <li><a href="https://elguendouz-ayoub.com/" class="text-green-500 hover:underline"> - Portfolio</a></li>
+          </ul>
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  svg {
+    margin-top: 20px;
+  }
+
+  ul {
+    text-align: left;
+  }
+
+  li {
+    width: 100%;
+    max-width: 450px;
+    margin: auto;
+  }
+
+  .container {
+    text-align: center;
+  }
+
+</style>
