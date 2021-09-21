c04b0a153e4dc1b7f2af05bff27b96991200ef33
TomymyFr <tomybrz@hotmail.fr>
2021-09-21 23:11:50 +0200

    add gradient mathilde

--- pages/mathilde.vue
+++ pages/mathilde.vue
@@ -1,5 +1,5 @@
 <template lang="html">
-    <div id="mathilde">
+    <div id="mathilde" class="bg-gradient-to-r from-pink-400 to-yellow-500" style="height: calc(100vh - 70px)">
         
     </div>
 </template>
