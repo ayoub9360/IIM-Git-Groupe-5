982b39137f3b1802c03b64577a9046b53c7f74b7
Ayoub El Guendouz <ayoub.el_guendouz@edu.devinci.fr>
2021-09-21 16:30:00 +0200

    Add navigation

---
+++ components/Nav.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="nav">
+    <nuxt-link to="Kevin">Kevin</nuxt-link>
+    <nuxt-link to="malo">malo</nuxt-link>
+    <nuxt-link to="tom">tom</nuxt-link>
+  </div>
+</template>
+
+<style scoped>
+  .nav {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    min-height: 70px;
+    background: rgb(216, 216, 216);
+  }
+
+  a {
+    color: black;
+    font-size: 18px;
+    font-weight: bold;
+    text-decoration: none;
+  }
+</style>
