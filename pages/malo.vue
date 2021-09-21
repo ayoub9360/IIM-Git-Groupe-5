c3a519c9e5a14e33283486800e420e8d9446aaac
Braunschweig Malo <malo.braunschweig@live.fr>
2021-09-21 16:45:19 +0200

    editing malo page

--- pages/malo.vue
+++ pages/malo.vue
@@ -1,7 +1,15 @@
 <template>
+<body>
   <div id="malo">
     <h1>Page de Malo</h1>
+    <br>
+    <h2>Bonjour à toi jeune entrepreneur,</h2>
+    <p>Si tu es sur ce site aujourd'hui c'est pour apprendre les techniques de pointes sur github, firebase ou encore vue.js
+        et tu as bien choisi, car ce site c'est le meilleur, le plus esthétique mais aussi le plus leger du marché.<p>
+
+    <img src="">
   </div>
+  </body>
 </template>
 <script>
     export default{}
