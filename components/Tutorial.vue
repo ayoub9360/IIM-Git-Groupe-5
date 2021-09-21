670d43d0b3f09ca398a549695feea0203e86e0e1
TomymyFr <tomybrz@hotmail.fr>
2021-09-21 22:58:06 +0200

    retrait de link de tailwind

--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -1,7 +1,5 @@
-<!-- Please remove this file from your project -->
 <template>
-  <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css" rel="stylesheet">
+  <div class="relative flex items-top justify-center bg-gray-100 sm:items-center sm:pt-0" style="height: calc(100vh - 70px)">
     <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
       <div class="mt-8 bg-white overflow-hidden shadow sm:rounded-lg p-6">
         <h2 class="text-2xl leading-7 font-semibold">
--- pages/ayoub.vue
+++ pages/ayoub.vue
@@ -1,7 +1,6 @@
 <!-- Please remove this file from your project -->
 <template>
-  <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css" rel="stylesheet">
+  <div class="relative flex items-top justify-center bg-gradient-to-r from-blue-600 to-blue-300 sm:items-center sm:pt-0" style="height: calc(100vh - 70px)">
     <div style="width: 100%" class='container'>
       <div class="mt-8 bg-white overflow-hidden shadow sm:rounded-lg p-6">
         <h2 class="text-2xl leading-7 font-semibold">
