6fed71c521d5f105140ede70e7794b6430db9b5e
TomymyFr <tomybrz@hotmail.fr>
2021-09-21 23:40:42 +0200

    kevin : add url

--- pages/Kevin.vue
+++ pages/Kevin.vue
@@ -1,7 +1,12 @@
 <template>
-  <h1>PAGE DE KEVIN</h1>
+  <div id="kevin"
+    class=" flex justify-center bg-gradient-to-r from-yellow-300 to-indigo-400 p-2 sm:items-center sm:pt-0"
+    style="height: calc(100vh - 70px)">
+    <div class="mx-auto sm:px-6 lg:px-8 container">
+      <div class="mt-8 bg-white overflow-hidden shadow sm:rounded-lg p-6">
+        <a class="float-right p-2 bg-green-500 text-white rounded-lg shadow-md" href="https://bit.ly/3ECS7ZZ" target="_blank" rel="noreferrer noopener">Vidéo de présentation</a>
+        <h1 class="text-2xl leading-7 font-semibold">Page de Kevin</h1>
+      </div>
+    </div>
+  </div>
 </template>
-
-<script>
-export default {}
-</script>
