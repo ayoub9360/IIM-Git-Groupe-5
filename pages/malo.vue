24b1cb7f5dcf5e2661baa4d4a729f2619feefc06
TomymyFr <tomybrz@hotmail.fr>
2021-09-21 23:40:51 +0200

    malo : add url

--- pages/malo.vue
+++ pages/malo.vue
@@ -1,14 +1,11 @@
 <template>
-  <div id="malo">
-    <h1>Page de Malo</h1>
+  <div id="malo" class=" flex justify-center bg-gradient-to-r from-yellow-300 to-indigo-400 p-2 sm:items-center sm:pt-0"
+    style="height: calc(100vh - 70px)">
+    <div class="mx-auto sm:px-6 lg:px-8 container">
+      <div class="mt-8 bg-white overflow-hidden shadow sm:rounded-lg p-6">
+        <a class="float-right p-2 bg-green-500 text-white rounded-lg shadow-md" href="https://bit.ly/3AAseaI" target="_blank" rel="noreferrer noopener">Vidéo de présentation</a>
+        <h1 class="text-2xl leading-7 font-semibold">Page de Malo</h1>
+      </div>
+    </div>
   </div>
 </template>
-<script>
-    export default{}
-</script>
-<style>
-  body {
-    margin: 0;
-    font-family: Arial;
-  }
-</style>
