8c53bcebbac208e452ba47affe01eacb8552fc83
Kevin <kevin.ho@edu.devinci.fr>
2021-09-22 12:18:18 +0200

    test done

---
+++ store/Kevin.vue
@@ -0,0 +1,32 @@
+<template>
+    <body>
+        <h1>Page de Kevin</h1>
+        <h2>Bienvenue sur ma page</h2>
+        <p>
+            Je m'appel Kevin, j'ai 21 ans actuellement en 3ème année a iim en developpement web. <br>
+            J'aime beaucoup le côté front mais également le back, plus tard j'ai l'intention d'être full-stack ! <br>
+        </p>
+
+
+    </body>
+</template>
+
+<style>
+  h1 {
+     text-align: center;
+     color: lightblue; 
+     font-family: Verdana, Geneva, Tahoma, sans-serif;
+     font-weight: 300;
+  }
+
+  h2 {
+      color: lightcoral;
+      font-family: Verdana, Geneva, Tahoma, sans-serif;
+      font-weight: 200;
+  }
+  
+</style>
+
+<script>
+export default {}
+</script>
