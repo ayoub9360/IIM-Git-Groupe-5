3c6e729e2050874cfefa9be39ec196e16de1e4a6
Braunschweig Malo <malo.braunschweig@live.fr>
2021-09-22 10:49:03 +0200

    Updating Malo Page

--- pages/malo.vue
+++ pages/malo.vue
@@ -5,6 +5,21 @@
       <div class="mt-8 bg-white overflow-hidden shadow sm:rounded-lg p-6">
         <a class="float-right p-2 bg-yellow-600 text-white rounded-lg shadow-md" href="https://bit.ly/3AAseaI" target="_blank" rel="noreferrer noopener">September 🎶</a>
         <h1 class="text-2xl leading-7 font-semibold">Page de Malo</h1>
+        <h2 class="text-2xl leading-7 font-semibold">
+          Hey its Malo
+        </h2>
+        <p class="mt-3 text-gray-600">
+          I am currently in third Year in IIM
+        </p>
+        <p class="mt-4 pt-4 text-green-800 border-t border-dashed">
+          <ul>
+            <li>Im a huge fan of devlopment and music, and some day i will contribute to music by developing</a></li>
+            <li>feel free to contact me at malodevweb@gmail.com</li>
+            <li>Wanna see me sing ? go for my instagram @SingingMalo</a></li>
+          </ul>
+        </p>
+        <p class="mt-4 pt-4 text-green-800 border-t border-dashed">
+          
       </div>
     </div>
   </div>
